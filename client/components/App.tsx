c57b0b30853d90d726125101d01714f32ad86b03
Oscar McGoldrick <oscar.mcgoldrick99@gmail.com>
2023-06-07 16:46:25 +1200

    header component added lololol

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -4,11 +4,13 @@ import { getWelcome } from '../apiClient'
 import cities from '../../data/cities'
 import { getCityTemps } from '../api/weatherApi'
 
+import { Header } from './Header'
 import Nav from './Nav'
 
 function App() {
 
   return <>
+  <Header />
   <Nav />
   <Outlet />
   </>
---
+++ client/components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header () {
+  return <>
+  <header>
+    <h1>
+      Wagwan it's the flipping weather 
+    </h1>
+  </header>
+  </>
+}
